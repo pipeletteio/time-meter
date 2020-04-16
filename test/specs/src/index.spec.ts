7cef7dcd6cea569768d76af62740d90f18c85294
AshLePoney <ashara.web@gmail.com>
2020-04-16 18:56:38 +0200

    feat: add TimeMeter and tests

--- test/specs/src/index.spec.ts
+++ test/specs/src/index.spec.ts
@@ -1,5 +1,29 @@
-import main from '../../../src/index';
+import { TimeMeter } from '../../../src/index';
 
-test('Should not be null', () => {
-  expect(main).not.toBe(null);
+test('should TimeMeter work fine', (done) => {
+  const timeMeter = new TimeMeter(process.hrtime());
+
+  // Wait for 1 second.
+  setTimeout(() => {
+    let seconds, nanoseconds;
+
+    [seconds] = timeMeter.next();
+    expect(seconds).toBe(1);
+
+    [seconds, nanoseconds] = timeMeter.next();
+    expect(seconds).toBe(0);
+    expect(nanoseconds).toBeGreaterThan(0);
+
+    // Wait for 0.5 second.
+    setTimeout(() => {
+      [seconds] = timeMeter.next();
+      expect(seconds).toBe(0);
+
+      [seconds, nanoseconds] = timeMeter.next();
+      expect(seconds).toBe(0);
+      expect(nanoseconds).toBeGreaterThan(500);
+
+      done();
+    }, 500);
+  }, 1000);
 });
